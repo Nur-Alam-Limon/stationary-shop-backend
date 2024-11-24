bd7237d79d0b3aa41635e5fda769d173c1cebe3a
Nur-Alam-Limon <nuralam201425@gmail.com>
2024-11-24 15:02:05 +0600

    Initialized a project with express, mongo, eslint, prettier, cors and other packages

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+// eslint.config.mjs
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    ignores: ['node_modules/*', 'dist/*'],
+  },
+
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+    },
+    globals: {
+      process: 'readonly',
+    },
+  },
+  eslintPluginPrettierRecommended,
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
---
+++ readme.md
@@ -0,0 +1,16 @@
+Steps to create project:
+1. Create project folder
+1. npm init -y
+2. npm i express
+3. npm install mongoose --save
+4. npm install typescript --save-dev
+5. npm i cors
+6. npm i dotenv
+7. tsc --init
+
+Package Installed:
+1. express
+2. mongoose
+3. typescript
+4. cors
+5. dotenv
